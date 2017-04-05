a8759579982f72e0aae1ca80ee70693f9dd7b735
Simon Hellinger <simon.hellinger@cgm.com>
2017-04-05 17:04:27 +0200

    Assignment 1_15 work in progress

--- 1_15/frequency.erl
+++ 1_15/frequency.erl
@@ -6,59 +6,110 @@
 %%   http://www.erlangprogramming.org/
 %%   (c) Francesco Cesarini and Simon Thompson
 
+
+%% Usage:
+%% To start a server that answers quickly, issue
+%% in the console:
+%% 
+%%    frequency:start(). 
+%%
+%% For a server that delays 3500ms 
+%% before each answer, type this into the console:
+%%
+%%    frequency:start(3500).
+%%
+%% Running frequency:allocate() a few times in quick succession
+%% will not produce any replies from the server until it's
+%% too late. When the 3500ms have passed, call frequency:allocate()
+%% again and you'll see the clear function throw out
+%% a series of allocation replies.
+
+%% FIXME: Is there a better place for clear/0 ???
+
 -module(frequency).
--export([start/0,allocate/0,deallocate/1,stop/0]).
--export([init/0]).
+-export([start/0,start/1,allocate/0,deallocate/1,stop/0]).
+-export([init/0,init/1]).
 
 %% These are the start functions used to create and
 %% initialize the server.
+
+% starts a regular server with no delays
 
 start() ->
     register(frequency,
 	     spawn(frequency, init, [])).
 
+% starts a slow server that delays every reply by DelayInMs
+
+start(DelayInMs) ->
+    register(frequency,
+	     spawn(frequency, init, [DelayInMs])).
+
 init() ->
+  init(0).
+
+init(DelayInMs) ->
   Frequencies = {get_frequencies(), []},
-  loop(Frequencies).
+  loop(Frequencies, DelayInMs).
 
 % Hard Coded
 get_frequencies() -> [10,11,12,13,14,15].
 
+get_receive_timeout_ms() -> 500.
+
 %% The Main Loop
 
-loop(Frequencies) ->
+loop(Frequencies, DelayInMs) ->
   receive
     {request, Pid, allocate} ->
       {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+      timer:sleep(DelayInMs),
       Pid ! {reply, Reply},
-      loop(NewFrequencies);
+      loop(NewFrequencies, DelayInMs);
     {request, Pid , {deallocate, Freq}} ->
       NewFrequencies = deallocate(Frequencies, Freq),
+      timer:sleep(DelayInMs),
       Pid ! {reply, ok},
-      loop(NewFrequencies);
+      loop(NewFrequencies, DelayInMs);
     {request, Pid, stop} ->
+      timer:sleep(DelayInMs),
       Pid ! {reply, stopped}
   end.
 
 %% Functional interface
 
-allocate() -> 
-    frequency ! {request, self(), allocate},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+% Each functional interface cleans out old messages before
+% issuing a new command.
 
-deallocate(Freq) -> 
-    frequency ! {request, self(), {deallocate, Freq}},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+allocate() ->
+  clear_verbose(),
+  frequency ! {request, self(), allocate},
+  receive 
+    {reply, Reply} -> 
+      Reply
+    after get_receive_timeout_ms() ->
+      {error, timeout}
+  end.
 
-stop() -> 
-    frequency ! {request, self(), stop},
-    receive 
-	    {reply, Reply} -> Reply
-    end.
+deallocate(Freq) ->
+  clear_verbose(),
+  frequency ! {request, self(), {deallocate, Freq}},
+  receive 
+    {reply, Reply} -> 
+      Reply
+    after get_receive_timeout_ms() ->
+      {error, timeout}
+  end.
+
+stop() ->
+  clear_verbose(),
+  frequency ! {request, self(), stop},
+  receive 
+    {reply, Reply} -> 
+      Reply
+    after get_receive_timeout_ms() ->
+      {error, timeout}
+  end.
 
 
 %% The Internal Help Functions used to allocate and
@@ -72,3 +123,26 @@ allocate({[Freq|Free], Allocated}, Pid) ->
 deallocate({Free, Allocated}, Freq) ->
   NewAllocated=lists:keydelete(Freq, 1, Allocated),
   {[Freq|Free],  NewAllocated}.
+
+%% The clear function removes all 
+%% messages from the mailbox and then exits.
+
+clear() ->
+	receive
+		_Msg ->
+			clear()
+		after 0 ->
+			ok
+	end.
+
+%% The verbose clear function prints and removes all 
+%% messages from the mailbox and then exits.
+
+clear_verbose() ->
+  receive
+		Msg ->
+      io:format("Throwing out message ~w~n", [Msg]),
+			clear_verbose()
+		after 0 ->
+			ok
+	end.
